4011cabcdee72cf210f5895b4edce2557ca85d57
Matomaral <mateo.obando@correounivalle.edu.co>
2022-07-22 22:01:42 -0500

    Se añade la funcion para mover a un jugador

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ environment=np.array([  [0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0],
-                        [0,0,0,0,0,0,0,0],
-                        [0,0,0,0,0,0,0,0],
+                        [0,0,3,5,0,0,0,0],
+                        [0,0,0,0,1,2,0,0],
                         [0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0]])
 
@@ -102,4 +102,72 @@ def findPlayer(player):
                 playerPos[1]=j
     return playerPos
 
-print(possibleMoves(2))
+##Metodo que recibe un jugador y una direccion para poder moverlo
+def movePlayer(player,move):
+    playerPos = findPlayer(player)
+    global whiteHorseScore
+    global blackHorseScore
+    global environment
+ 
+    #Se hara el movimiento segun la direccion dada 
+    if(move == 0):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]-1,playerPos[1]-2]
+    if(move == 1):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]-2,playerPos[1]-1]
+    if(move == 2):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]-1,playerPos[1]+2]
+    if(move == 3):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]-2,playerPos[1]+1]
+    if(move == 4):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]+1,playerPos[1]+2]
+    if(move == 5):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]+2,playerPos[1]+1]
+    if(move == 6):
+        environment[playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]+1,playerPos[1]-2]
+    if(move == 7):
+        environment [playerPos[0],playerPos[1]] = 0
+        playerPos = [playerPos[0]+2,playerPos[1]-1]
+    
+    
+    auxPos = playerPos
+    #Si al moverse recoge un objeto adicionar los puntos necesarios
+    #Cesped = 3,flores = 4, manzanas = 5
+    #Valen 1,3 y 5 puntos respectivamente
+    if(environment[auxPos[0]][auxPos[1]]==3):
+        if(player == 1):
+            blackHorseScore = blackHorseScore + 1
+        else:
+            whiteHorseScore = whiteHorseScore + 1
+    if(environment[auxPos[0]][auxPos[1]]==4):
+        if(player == 1):
+            blackHorseScore = blackHorseScore + 3
+        else:
+            whiteHorseScore = whiteHorseScore + 3
+    if(environment[auxPos[0]][auxPos[1]]==5):
+        if(player == 1):
+            blackHorseScore = blackHorseScore + 5
+        else:
+            whiteHorseScore = whiteHorseScore + 5
+    environment[auxPos[0],auxPos[1]]=player
+    return environment
+
+
+
+
+
+
+print(blackHorseScore,whiteHorseScore)
+print(environment)
+print("----------------------------------------------------------------------------------------")
+print(movePlayer(1,0))
+print("----------------------------------------------------------------------------------------")
+print(movePlayer(2,0))
+print(blackHorseScore,whiteHorseScore)
+
