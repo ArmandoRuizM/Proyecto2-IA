985a389da58bbce8e037e79146b3ec8d30d01a51
Daniel Arango <daniel.rodas@correounivalle.edu.co>
2022-07-24 15:26:19 -0500

    Refactorización del código para generar tablero.

--- tablero.py
+++ tablero.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-#Función que crea el tablero inicial
+#Función que crea un tablero inicial
 #def tablero_inicial():
     #board = []
     #for i in range(0, 8):
@@ -10,11 +10,7 @@ import numpy as np
         #board.append(line)
     #return board
 
-# Arreglo para almacenar el número total de items, donde items[0] son la cantidad de jugadores, 
-# items[1] la cantidad de césped, items[2] la cantidad de flores e items[3] la cantidad de manzanas
-
-items = [2, 14, 5, 2]
-
+# Tablero inicial
 tablero_inicial = np.array([  [0,0,0,0,0,0,0,0],
                               [0,0,0,0,0,0,0,0],
                               [0,0,0,0,0,0,0,0],
@@ -23,39 +19,32 @@ tablero_inicial = np.array([  [0,0,0,0,0,0,0,0],
                               [0,0,0,0,0,0,0,0],
                               [0,0,0,0,0,0,0,0],
                               [0,0,0,0,0,0,0,0]])
+# Arreglo para almacenar el número total de items, donde items[1] la cantidad de césped, 
+# items[2] la cantidad de flores e items[3] la cantidad de manzanas                     
+items = [14, 5, 2]
+                       
+def randomObjects(objetos, cantidad, item):
+     while objetos < cantidad:
+        x = random.randint(0,7)
+        y = random.randint(0,7)
+        if tablero_inicial[x][y] == 0:
+            tablero_inicial[x][y] = item
+            objetos += 1
+
+def randomPlayers(numJugador):
+    while True:
+        x = random.randint(0,7)
+        y = random.randint(0,7)
+        if tablero_inicial[x][y] == 0:
+            tablero_inicial[x][y] = numJugador
+            return False
 
 def tablero():
     cesped = flores = manzanas = 0
-    jugador1 = jugador2 = True
-    
-    while jugador1:
-        x = random.randint(0,7)
-        y = random.randint(0,7)
-        if tablero_inicial[x][y] == 0:
-            tablero_inicial[x][y] = 1
-            jugador1 = False
-    while jugador2:
-        x = random.randint(0,7)
-        y = random.randint(0,7)
-        if tablero_inicial[x][y] == 0:
-            tablero_inicial[x][y] = 2
-            jugador2 = False
-    while cesped < items[1]:
-        x = random.randint(0,7)
-        y = random.randint(0,7)
-        if tablero_inicial[x][y] == 0:
-            tablero_inicial[x][y] = 3
-            cesped += 1
-    while flores < items[2]:
-        x = random.randint(0,7)
-        y = random.randint(0,7)
-        if tablero_inicial[x][y] == 0:
-            tablero_inicial[x][y] = 4
-            flores += 1
-    while manzanas < items[3]:
-        x = random.randint(0,7)
-        y = random.randint(0,7)
-        if tablero_inicial[x][y] == 0:
-            tablero_inicial[x][y] = 5
-            manzanas += 1
+    randomPlayers(1)
+    randomPlayers(2)
+    randomObjects(cesped, items[0], 3)
+    randomObjects(flores, items[1], 4)
+    randomObjects(manzanas, items[2], 5)
+
     return tablero_inicial    
